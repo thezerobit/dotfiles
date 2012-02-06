d776f3bbc4d11b17ae57a0406deb9eaa1741e851
Stephen A. Goss <steveth45@gmail.com>
2012-02-06 23:40:29 +0000

    Added work, updated make_links.py to ask which profile

--- make_links.py
+++ make_links.py
@@ -8,55 +8,70 @@ copyright 2010 Stephen A. Goss
 from __future__ import print_function
 
 import os, shutil, stat
-
-dotfiles_dir = os.path.dirname(os.path.realpath(__file__))
-home_dir = os.path.expanduser("~")
+from os.path import (dirname, realpath, join, isdir, isfile, expanduser)
 
 def should_ask(filename):
     return filename.startswith('.bash') or filename.endswith('profile')
 
-print("dotfiles in this folder : %s" % dotfiles_dir)
-dotfiles = set(os.listdir(dotfiles_dir))
-dotfiles = dotfiles - set(['.git', 'make_links.py', '.gitignore', 'README.md', 'configurations','profiles'])
-dotfiles = list(dotfiles)
-print("\n".join(dotfiles))
-print("target home folder : %s" % home_dir)
-backup_folder_path = os.path.join(home_dir, "backups")
-if not os.path.isdir(backup_folder_path):
-    print("creating backup folder : %s" % backup_folder_path)
-    os.mkdir(backup_folder_path)
-for filename in dotfiles:
-    if should_ask(filename):
-        answer = raw_input('Symlink %s ? ' % filename)
-        if not answer in ['y', 'Y']:
-            continue
-    print("processing %s ..." % filename)
-    target = os.path.join(home_dir, filename)
-    dotfile = os.path.join(dotfiles_dir, filename)
-    try:
-        statinfo = os.lstat(target)
-    except:
-        print("file does not exist : %s" % target)
-        statinfo = None
-    if statinfo:
-        if stat.S_ISLNK(statinfo.st_mode):
-            print("skipping symlink : %s" % target)
-            continue
-        else:
-            backup = os.path.join(backup_folder_path, filename)
-            if stat.S_ISDIR(statinfo.st_mode):
-                if not os.path.isdir(backup):
-                    print("copying (folder) %s to %s " % (target, backup))
-                    shutil.copytree(target, backup)
-                print("deleting folder: %s" % target)
-                shutil.rmtree(target)
-            else:
-                if not os.path.isfile(backup):
-                    print("copying %s to %s " % (target, backup))
-                    shutil.copyfile(target, backup)
-                print("delete file : %s" % target)
+def link_files(files, file_dir):
+    home_dir = expanduser("~")
+    backup_folder_path = join(home_dir, "backups")
+    if not isdir(backup_folder_path):
+        print("creating backup folder : %s" % backup_folder_path)
+        os.mkdir(backup_folder_path)
+    for filename in files:
+        if should_ask(filename):
+            answer = raw_input('Symlink %s ? ' % filename)
+            if not answer in ['y', 'Y']:
+                continue
+        print("processing %s ..." % filename)
+        target = join(home_dir, filename)
+        dotfile = join(file_dir, filename)
+        try:
+            statinfo = os.lstat(target)
+        except:
+            print("file does not exist : %s" % target)
+            statinfo = None
+        if statinfo:
+            if stat.S_ISLNK(statinfo.st_mode):
+                print("removing old symlink : %s" % target)
                 os.remove(target)
-    print("creating symlink : %s to %s" % (dotfile,target))
-    os.symlink(dotfile, target)
+            else:
+                backup = join(backup_folder_path, filename)
+                if stat.S_ISDIR(statinfo.st_mode):
+                    if not isdir(backup):
+                        print("copying (folder) %s to %s " % (target, backup))
+                        shutil.copytree(target, backup)
+                    print("deleting folder: %s" % target)
+                    shutil.rmtree(target)
+                else:
+                    if not isfile(backup):
+                        print("copying %s to %s " % (target, backup))
+                        shutil.copyfile(target, backup)
+                    print("delete file : %s" % target)
+                    os.remove(target)
+        print("creating symlink : %s to %s" % (dotfile,target))
+        os.symlink(dotfile, target)
 
-print("done!")
+if __name__ == '__main__':
+    dotfiles_dir = dirname(realpath(__file__))
+    print("dotfiles in this folder : %s" % dotfiles_dir)
+    dotfiles = set(os.listdir(dotfiles_dir))
+    dotfiles = dotfiles - set(['.git', 'make_links.py', '.gitignore', 'README.md', 'configurations','profiles'])
+    dotfiles = list(dotfiles)
+    print("\n".join(dotfiles))
+
+    link_files(dotfiles, dotfiles_dir)
+
+    profiles_dir = join(dirname(realpath(__file__)), 'profiles')
+    profiles = os.listdir(profiles_dir)
+    profile_name = raw_input("Select profile (" + " ".join(profiles) + "): ")
+    prof_dir = join(profiles_dir, profile_name)
+    if isdir(prof_dir):
+        prof_files = set(os.listdir(prof_dir))
+        print("Linking profile files...")
+        link_files(prof_files, prof_dir)
+    else:
+        print("Error: invalid profile.")
+
+    print("done!")

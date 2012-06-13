7078ea396f49c4ae9dba6db7de1b539d4dda08aa
Stephen A. Goss <steveth45@gmail.com>
2012-06-12 20:05:33 -0700

    remove emacs

--- .emacs.d/init.el
+++
@@ -1,7 +0,0 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
-(defun turn-on-paredit () (paredit-mode 1))
-(add-hook 'clojure-mode-hook 'turn-on-paredit)
-
